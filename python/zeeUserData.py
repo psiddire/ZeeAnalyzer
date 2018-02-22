d94b1fd4f4de19f5cdeaf405e4c0d6629b889888
Prasanna Siddireddy <psiddire@nd.edu>
2018-02-22 19:31:52 +0100

    Removing Unnecessary Files

--- python/zeeUserData.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-import copy
-
-userDataElectrons = cms.EDProducer(
-    "ZEEUserData",
-    src = cms.InputTag("slimmedElectrons"),
-    inputRho = cms.InputTag("fixedGridRhoFastjetAll")
-    )
-
